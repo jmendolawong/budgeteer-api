406391d6f79b09db1e46993418f1139311224601
mendolawong <jwong.ncsu@gmail.com>
2020-04-15 15:36:46 -0400

    Deployment ready

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config.js')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config.js')
 
 const expenseRouter = require('./expenses/expense-router')
 
@@ -12,7 +12,11 @@ const app = express();
 
 /***********  Middleware ***********/
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 const morganSetting = NODE_ENV === 'production' ? 'tiny' : 'dev';
 app.use(morgan(morganSetting))
 
