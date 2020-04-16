ef7ef392bd672d2865f57648bb655b07839dc19c
mendolawong <jwong.ncsu@gmail.com>
2020-04-16 10:18:58 -0400

    Fixed package.json for deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 
-module.exports ={
-  "migrationsDirectory": "migrations",
+module.exports = {
+  "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
